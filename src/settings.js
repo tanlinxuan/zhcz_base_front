9f6250817f18dfd77483caecef1f548c3727b2f0
tanlinxuan <517765002@qq.com>
2020-07-01 16:12:06 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: '招商平台',
+  showSettings: true,   //是否显示右侧悬浮配置按钮
+  tagsView: true,   //是否显示页面标签功能条
+  fixedHeader: false,   //是否将头部布局固定
+  sidebarLogo: false,   //菜单栏中是否显示LOGO
+  errorLog: 'production' //默认显示错误日志的环境
+}
